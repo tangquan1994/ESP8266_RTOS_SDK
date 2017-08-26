68c1fd70dd476a80452ac48c4dedf448a23a38bb
tangquan <root@ubuntu.(none)>
2017-08-25 19:38:06 -0700

    Initial commit

---
+++ driver_lib/driver/.output/eagle/debug/obj/i2c_master.d
@@ -0,0 +1,11 @@
+.output/eagle/debug/obj/i2c_master.o .output/eagle/debug/obj/i2c_master.d : i2c_master.c \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/c_types.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/ets_sys.h \
+ ../include/gpio.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/gpio_register.h \
+ ../include/i2c_master.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/pin_mux_register.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/eagle_soc.h
---
+++ driver_lib/driver/.output/eagle/debug/obj/spi_interface.d
@@ -0,0 +1,11 @@
+.output/eagle/debug/obj/spi_interface.o .output/eagle/debug/obj/spi_interface.d : spi_interface.c ../include/spi_interface.h \
+ ../include/spi_register.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/c_types.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /root/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/eagle_soc.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/ets_sys.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/pin_mux_register.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp8266/eagle_soc.h \
+ /mnt/hgfs/VMShare/ESP8266/ESP8266_RTOS_SDK-master/include/espressif/esp_libc.h
