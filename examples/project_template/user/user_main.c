49c1a8f4a547b6bbdc133c82ddef2760db05f0b6
tangquan1994 <384998430@qq.com>
2017-08-27 07:58:06 -0700

    set uart1 as print piort uart0 as comunication port

--- examples/project_template/user/user_main.c
+++ examples/project_template/user/user_main.c
@@ -349,9 +349,19 @@ void UART_intr_handler()
 *******************************************************************************/
 void user_init(void)
 {	
+    UART_ConfigTypeDef uart_config;
+    uart_config.baud_rate	= BIT_RATE_115200;
+    uart_config.data_bits	= UART_WordLength_8b;
+    uart_config.parity		= USART_Parity_None;
+    uart_config.stop_bits	= USART_StopBits_1;
+    uart_config.flow_ctrl	= USART_HardwareFlowControl_None;
+    uart_config.UART_RxFlowThresh = 120;
+    uart_config.UART_InverseMask = UART_None_Inverse;
+    UART_ParamConfig(UART1, &uart_config);
+    
 	UART_SetBaudrate(UART0,BIT_RATE_115200);
-	UART_SetPrintPort(UART0);
-//	UART_SetIntrEna(UART0,UART_INTR_MASK);
+	UART_SetBaudrate(UART1,BIT_RATE_115200);
+	UART_SetPrintPort(UART1);	//串口1用于打印调试信息，串口0用于数据传输
 	ETS_UART_INTR_ENABLE();
 	UART_intr_handler_register(UART_intr_handler,NULL);
 	
